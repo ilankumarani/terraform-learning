08df08eb729ab9902d7661d89f52ea137b141a6f
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2025-11-27 20:51:07 +0000

    test

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,6 +1,6 @@
 module "iam_admin" {
   # Git repo + subfolder + branch
-  source = "git::https://github.com/ilankumarani/terraform-learning.git//modules/iam-admin?ref=main"
+  source = "git::https://github.com/ilankumarani/terraform-aws-modules.git//iam-admin?ref=main"
 
   admin_group_name            = "TERRAFORM_ADMIN_GROUP_PROD"
   usernames                   = ["terraform_user_prod1"]
