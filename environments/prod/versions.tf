686e7fc8b63deee4456c7c889d2b945ec7f75fe2
ilankumaran <kumaranilan@yahoo.com>
2025-12-01 15:46:26 +0000

    test

--- environments/prod/versions.tf
+++ environments/prod/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.6.0" # Terraform version, if this property not provided, then it always takes the latest
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "6.23.0" # AWS provider plugin version. At this point in time, this is the latest
     }
   }
 }
