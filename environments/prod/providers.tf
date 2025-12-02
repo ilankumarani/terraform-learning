523f9f83bcbe4cdd712bd6c0bb61c3d5dad3d879
ilankumaran <kumaranilan@yahoo.com>
2025-12-02 12:40:11 +0000

    test

--- environments/prod/providers.tf
+++ environments/prod/providers.tf
@@ -1,4 +1,4 @@
 provider "aws" {
-  region = "eu-west-1"
-  profile = "default"
+  region = "eu-west-1" # Optional to provide the region
+  profile = "default" # Profile name to use the Authentication from C:\Users\<your-username>\.aws\credentials
 }
